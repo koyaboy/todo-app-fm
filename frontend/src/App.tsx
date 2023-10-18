538ff109958a707917a2e3fb0e9499411cea0164
Koya <44787464+koyaboy@users.noreply.github.com>
2023-10-18 19:31:59 +0100

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-red">
+      App
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'red': '#FF0000',
+
+      primary: {
+        'bright-blue': 'hsl(220, 98%, 61%)',
+        'check-background': 'linear-gradient(90deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))',
+
+      },
+
+      lightMode: {
+        'very-light-gray': 'hsl(0, 0%, 98%)',
+        'very-light-grayish-blue': 'hsl(236, 33%, 92%)',
+        'light-grayish-blue': 'hsl(233, 11%, 84%)',
+        'dark-grayish-blue': 'hsl(236, 9%, 61%)',
+        'very-dark-grayish-blue': 'hsl(235, 19%, 35%)',
+      },
+
+      darkMode: {
+        'very-dark-blue': 'hsl(235, 21%, 11%)',
+        'very-dark-desaturated-blue': 'hsl(235, 24%, 19%)',
+        'light-grayish-blue': 'hsl(234, 39%, 85%)',
+        'dark-grayish-blue': 'hsl(234, 11%, 52%)',
+        'very-dark-grayish-blue': 'hsl(233, 14%, 35%)',
+        'very-dark-grayish-blue': 'hsl(237, 14%, 26%)',
+      }
+    }
+  },
+  plugins: [],
+}
