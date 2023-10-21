0cfe42a19b768320adddcfdd1689d146ff0cedc0
Koya <44787464+koyaboy@users.noreply.github.com>
2023-10-22 00:10:26 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ require("dotenv").config()
 
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 const todoRoutes = require("./routes/todos")
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
